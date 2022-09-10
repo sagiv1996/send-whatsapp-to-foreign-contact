cbb4c31e6991f43b438426482ceec7510fbd6b3d
sagiv bonali <sagivb13@gmail.com>
2022-09-10 21:52:44 +0300

    first version (have more a bit bugs)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -11,105 +14,91 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Send whatsapp to foreign contact',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page - iam change the text '),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String? phoneNumber;
+  bool isValid = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<void> sendAWhatsappMessage() async {
+    if (!isValid) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: new Text('The phone number is not a valid.')));
+      return;
+    }
+    try {
+      await launchUrl(getUriToWhatsApp());
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: new Text('The system failed to open WhatsApp')));
+    }
   }
+
+  Uri getUriToWhatsApp() => Uri.parse('whatsapp://send?phone=$phoneNumber');
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+        body: Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromRGBO(18, 140, 126, 0.7),
+            Color.fromRGBO(7, 94, 84, 0.6),
+            Color.fromRGBO(37, 211, 102, 1),
+            Color.fromRGBO(52, 183, 241, 0.7),
           ],
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      child: Center(
+        child: //
+            InternationalPhoneNumberInput(
+          locale: 'il',
+          onInputChanged: (phoneNumber) {
+            setState(() {
+              this.phoneNumber = phoneNumber.toString();
+            });
+          },
+          // Add this value because onInputChanged is required
+          onInputValidated: (bool isValid) async {
+            if (isValid) {
+              setState(() {
+                this.isValid = isValid;
+              });
+              sendAWhatsappMessage();
+            }
+          },
+          onFieldSubmitted: (phoneNumber) {
+            setState(() {
+              this.phoneNumber = phoneNumber;
+              sendAWhatsappMessage();
+            });
+          },
+          selectorConfig: SelectorConfig(
+              selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
+              useEmoji: true,
+              showFlags: true),
+          autoValidateMode: AutovalidateMode.onUserInteraction,
+          selectorTextStyle: TextStyle(color: Colors.black),
+          inputBorder: OutlineInputBorder(),
+        ),
+      ),
+    ));
   }
 }
